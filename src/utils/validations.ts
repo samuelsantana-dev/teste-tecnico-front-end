61d4f204a2b836b65fb81c5c3e01591752801543
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-09-12 11:05:42 -0300

    Refactor validations zod

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,23 +1,57 @@
 import { z } from "zod";
-export const loginSchema = z.object({
-    email: z.email("Email inválido"),
-    password: z.string().min(5, "Senha deve ter no mínimo 6 caracteres"),
-})
 
-export const registerSchema = z.object({
-  name: z.string().min(3, "Nome deve ter no mínimo 3 caracteres"),
-  email: z.string().email("Email inválido"),
-  password: z.string().min(6, "Senha deve ter no mínimo 6 caracteres"),
-  verifyPassword: z.string().min(6, "Confirmação de senha deve ter no mínimo 6 caracteres"),
-  phone: z.object({
-    country: z.string().min(1, "Código do país é obrigatório"),
-    ddd: z.string().min(2, "DDD inválido").max(3, "DDD inválido"),
-    number: z.string().min(8, "Número inválido"),
-  }),
-}).refine((data) => data.password === data.verifyPassword, {
-  message: "As senhas não conferem",
-  path: ["verifyPassword"],
+export const loginSchema = z.object({
+  email: z
+    .string()
+    .email("Digite um email válido"),
+  password: z
+    .string()
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,
+      "Senha deve conter ao menos uma letra maiúscula, uma minúscula, um número e um caractere especial"
+    ),
 });
+
+
+export const registerSchema = z
+  .object({
+    name: z
+      .string()
+      .min(3, "Nome deve ter no mínimo 3 caracteres")
+      .regex(/^[A-Za-zÀ-ÿ\s]+$/, "Nome deve conter apenas letras e espaços"),
+    email: z.string().email("Email inválido"),
+    password: z
+      .string()
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,
+        "Senha deve conter ao menos uma letra maiúscula, uma minúscula, um número e um caractere especial"
+      ),
+    verifyPassword: z.string().regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/,
+        "Senha deve conter ao menos uma letra maiúscula, uma minúscula, um número e um caractere especial"
+      ),
+    phone: z.object({
+      country: z
+        .string()
+        .min(1, "Código do país é obrigatório")
+        .regex(/^\d+$/, "Código do país inválido"),
+      ddd: z
+        .string()
+        .min(2, "DDD inválido")
+        .max(3, "DDD inválido")
+        .regex(/^\d+$/, "DDD deve conter apenas números"),
+      number: z
+        .string()
+        .min(8, "Número inválido")
+        .max(10, "Número inválido")
+        .regex(/^\d+$/, "Número deve conter apenas números"),
+    }),
+  })
+  .refine((data) => data.password === data.verifyPassword, {
+    message: "As senhas não conferem",
+    path: ["verifyPassword"],
+  });
+
 
 export const productEditSchema = z.object({
   title: z.string().min(3, "Título deve ter no mínimo 3 caracteres"),
